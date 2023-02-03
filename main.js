4c313413b91bbd2282a8c3ce630a65dfa547ecf6
guadalupe uhalt <guadalupeuhalt@hotmail.com>
2023-02-03 18:55:40 -0300

    new apptareas

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
+// const tareas = [
+//   {
+//     Titulo: "Aprender Js",
+//     Estado: "Pendiente",
+//   }
+//   {
+//     Titulo: "Aprender CSS",
+//     Estado: "En Progreso",
+//   }
+// ]
+
+
 function $(elementoDeHtml) {
   return document.querySelector(elementoDeHtml);
 }
 
-const $openButton = document.querySelector("#open-modal");
+// window.addEventListener("load", () => {
+//   /*modal*/
+//   const $openButton = document.querySelector("#open-modal");
+// const $containModal = document.querySelector(".contain-modal");
+// const $closeButton = document.querySelector("#close-modal");
+
+
+// /*contenedores*/
+// const $containFilters = $ ".contain-filters";
+// const $containTareas = $ ".containTareas";
+
+window.addEventListener("load", () => {
+  const $openButton = document.querySelector("#open-modal");
 const $containModal = document.querySelector(".contain-modal");
 const $closeButton = document.querySelector("#close-modal");
 
@@ -19,3 +43,18 @@ $openButton.onClick = (event) => {
   event.preventDefault();
   $containModal.classList.add("show-modal");
 };
+
+})
+
+// function paint (nodo, datos) {
+//    nodo.innerHTML = "";
+//    datos.forEach (tarea => {
+//     nodo.innerHTML += 
+
+
+
+//    })
+// }
+// const tareasPendientes = tareas.filter 
+
+// })
